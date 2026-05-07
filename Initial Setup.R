0aba68f09f625496832ff641b8f2a410b8065c26
renee-sakura <reneesakura@icloud.com>
2026-05-07 19:42:09 +1000

    Initial Setup.R

--- Initial Setup.R
+++ Initial Setup.R
@@ -2,4 +2,4 @@
 #   Initial Setup for Waratahs_SuperW Dashboard
 # ============================================================
 
-install.packages(c("shiny","shinydashboard","dplyr","ggplot2","lubridate","scales","tidyr","stringr","readr","DT","tidyverse","catapultR","hms"))
+install.packages(c("shiny","shinydashboard","dplyr","ggplot2","lubridate","scales","tidyr","stringr","readr","DT","tidyverse","catapultR","hms","plotly","openxlsx"))
